42e379bc28dfe0eb15e40d76524df67fd080f660
s_chuli <stc9606@naver.com>
2021-12-30 23:43:05 +0900

    prometheus & grafana add

---
+++ mvc_study/src/main/java/com/example/mvc_study/MvcStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvc_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcStudyApplication.class, args);
+    }
+
+}
